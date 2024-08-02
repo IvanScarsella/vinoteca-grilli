c9c14b17c9dec2d9068c194e1edb3146d2721f2f
Iván Scarsella <scarsellaivan@gmail.com>
2024-08-02 12:04:33 -0300

    finish footer responsive

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import logo from '../../../public/logo.png'
 import instagramLogo from '../../../public/instagramLogo.png'
 import whatsappLogo from '../../../public/whatsappLogo.png'
+import Link from "next/link"
 
 export default function Footer() {
 
@@ -15,8 +16,8 @@ export default function Footer() {
    ]
 
    const socialMedia = [
-      { name: 'Instagram', src: instagramLogo },
-      { name: 'WhatsApp', src: whatsappLogo }
+      { name: 'Instagram', src: instagramLogo, href: 'https://www.instagram.com/grillivinoteca/' },
+      { name: 'WhatsApp', src: whatsappLogo, href: 'https://api.whatsapp.com/send/?phone=5492213995216' }
    ]
 
    const footerItems = [
@@ -26,27 +27,29 @@ export default function Footer() {
    ]
 
    return (
-      <footer className="h-96 w-full bg-gradient-to-b from-yellow1 to-[#987135] px-[50px] py-6 flex justify-between">
-         <div className="w-1/3 h-full flex flex-col justify-between">
+      <footer className="h-96 w-full bg-gradient-to-b from-yellow1 to-[#987135] px-[50px] py-6 flex justify-between max-md:flex-col max-md:p-1 max-md:py-2 max-md:gap-4 max-md:h-auto">
+         <div className="w-1/3 h-full flex flex-col justify-between max-md:flex-row max-md:w-full max-md:px-5">
             {footerButtons.map((button) => (
-               <p className="font-medium text-[32px]" key={button.title}>{button.title}</p>
+               <p className="font-medium text-[32px] max-md:text-xl max-[450px]:text-base" key={button.title}>{button.title}</p>
             )
             )}
          </div>
-         <div className="w-1/3 flex flex-col justify-around items-center pr-40">
-            <Image src={logo} alt='logo' className="h-[146px] w-[146px]" />
-            <p className="text-[15px] font-normal -mt-6">Vinoteca online</p>
-            <button className="text-xl p-2 rounded-lg border border-black1">Instrucciones de uso</button>
+         <div className="w-1/3 flex flex-col justify-around items-center lg:pr-40 min-[900px]:pr-24 md:pr-16 max-md:w-full max-md:min-h-[191px] max-md:gap-4">
+            <Image src={logo} alt='logo' className="size-[146px] min-w-[146px] max-sm:size-24 max-md:min-w-0" />
+            <p className="text-[15px] font-normal -mt-6 text-center">Vinoteca online</p>
+            <button className="text-xl p-2 rounded-lg border border-black1 max-md:text-sm max-md:border-[0.5px]">Instrucciones de uso</button>
          </div>
-         <div className="w-1/3 flex flex-col gap-8 justify-center items-center">
+         <div className="w-1/3 flex flex-col gap-8 justify-center items-center max-md:w-full">
             <div className="flex gap-9 justify-center">
                {socialMedia.map((media) => (
-                  <Image src={media.src} alt={media.name} className="h-12 w-12 cursor-pointer" />
+                  <Link href={media.href} target='_blank'>
+                     <Image src={media.src} alt={media.name} className="size-12 cursor-pointer max-md:size-10" />
+                  </Link>
                ))}
             </div>
-            <ul className="list-disc">
+            <ul className="list-disc ">
                {footerItems.map((item) => (
-                  <li className="text-[15px] font-normal">{item}</li>
+                  <li className="text-[15px] max-md:tracking-tight font-normal">{item}</li>
                ))}
             </ul>
          </div>
